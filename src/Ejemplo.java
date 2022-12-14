25c96515417774d47362282be7a72d1b09e3d35f
Carlos Pastor <cpastorgamero@safareyes.es>
2022-12-14 16:57:35 +0100

    Initial commit

---
+++ src/Ejemplo.java
@@ -0,0 +1,9 @@
+public class Ejemplo {
+    public static void ejmplo(){
+        System.out.println("ni de coña");
+    }
+
+    public static void main(String[] args) {
+        ejmplo();
+    }
+}
